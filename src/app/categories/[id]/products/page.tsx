00fe236f9a521e62cfaae2293ce4fc6fd4ad6339
Fabio Antonio <fabio.henrique100@hotmail.com>
2024-05-10 15:03:45 -0300

    feat: add category products page

---
+++ src/app/categories/[id]/products/page.tsx
@@ -0,0 +1,53 @@
+import Header from "@/components/header";
+import ProductItem from "@/app/products/[id]/components/product-item";
+import { db } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
+interface CategoriesPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
+  const category = await db.category.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      products: {
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!category) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">{category.name}</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {category.products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoriesPage;
