950ee0e91f1fa92a040d801b5ec598f18a9f43c5
Fabio Antonio <fabio.henrique100@hotmail.com>
2024-05-10 15:03:42 -0300

    feat: ensure juices from product restaurants are listed

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,13 +2,11 @@ import { db } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import ProductImage from "@/components/product-image";
 import ProductDetails from "@/components/product-details";
-
 interface ProductPageProps {
   params: {
     id: string;
   };
 }
-
 const ProductPage = async ({ params: { id } }: ProductPageProps) => {
   const product = await db.product.findUnique({
     where: {
@@ -19,10 +17,17 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
+  if (!product) {
+    return notFound();
+  }
+
   const juices = await db.product.findMany({
     where: {
       category: {
         name: "Sucos",
+      },
+      restaurant: {
+        id: product?.restaurant.id,
       },
     },
     include: {
@@ -30,19 +35,13 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
-  if (!product) {
-    return notFound();
-  }
-
   return (
     <div>
       {/* IMAGEM */}
       <ProductImage product={product} />
-
       {/* TITULO E PREÇO */}
       <ProductDetails product={product} complementaryProducts={juices} />
     </div>
   );
 };
-
 export default ProductPage;
