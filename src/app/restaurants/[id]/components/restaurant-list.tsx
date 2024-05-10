fcc7cb07e68b6a168a5a8d75756fe6a28bcccedb
Fabio Antonio <fabio.henrique100@hotmail.com>
2024-05-10 15:03:52 -0300

    feat: ensure restaurant can be favorited

--- src/app/restaurants/[id]/components/restaurant-list.tsx
+++ src/app/restaurants/[id]/components/restaurant-list.tsx
@@ -1,16 +1,30 @@
+import { getServerSession } from "next-auth";
 import { db } from "@/lib/prisma";
 import RestaurantItem from "./restaurant-item";
+import { authOptions } from "@/lib/auth";
 
 const RestaurantList = async () => {
+  const session = await getServerSession(authOptions);
+
   // TODO: pegar restaurantes com maior número de pedidos
   const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: { userId: session?.user?.id },
+  });
+
+  console.log(userFavoriteRestaurants);
+
   return (
     <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
-        <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+        <RestaurantItem
+          key={restaurant.id}
+          restaurant={restaurant}
+          userId={session?.user?.id}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
       ))}
     </div>
   );
 };
-
 export default RestaurantList;
