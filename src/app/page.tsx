031733a37e13dcb7ed9dcf6f464069e02bda343f
Fabio Antonio <fabio.henrique100@hotmail.com>
2024-05-10 15:03:37 -0300

    feat: add category item and list components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
 
@@ -8,6 +9,10 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
       </div>
     </>
   );
